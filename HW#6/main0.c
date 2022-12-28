9152544b3d05e6e1b6ef9f50f6f07cf470bcdea2
B1129011 <119965133+B1129011@users.noreply.github.com>
2022-12-28 10:12:31 +0800

    Update main0.c

--- HW#6/main0.c
+++ HW#6/main0.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 
-int checkPowerOf2(unsigned int num)
-{
-    // Check if the number has only one set bit
-    if ((num & (num - 1)) != 0)
-        return 0;
-    return 1;
-}
-
 int main()
 {
     int i = 10;
@@ -15,10 +7,10 @@ int main()
     printf("Enter Number: %d\n",i);
     
     
-    if (checkPowerOf2(i))
-        printf("%d is power of 2.\n",i);
+    if ((i & (i - 1)) != 0)
+        printf("%d is False.\n",i);
     else
-        printf("%d is not power of 2.\n",i);
+        printf("%d is True.\n",i);
 
     return 0;
 }
