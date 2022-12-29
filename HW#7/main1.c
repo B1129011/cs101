c06ee3b58ac6e6f73a2118f34fd7ff6e21c5ba7f
B1129011 <119965133+B1129011@users.noreply.github.com>
2022-12-29 11:21:24 +0800

    Update main1.c

--- HW#7/main1.c
+++ HW#7/main1.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     for (int i=1;i<8;i++){
         printf("%*s",8-i," ");
-        for(int j=0;j<i;j++){
-            printf("%d ",i);
-        }
+    for(int j=0;j<i;j++){
+        printf("%d ",i);
+    }
         printf("\n");
     } 
     return 0;
