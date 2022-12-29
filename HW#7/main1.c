3ce7cb7840a2f666d6d41a3db34564aeec90c9b8
B1129011 <119965133+B1129011@users.noreply.github.com>
2022-12-29 11:15:27 +0800

    Update main1.c

--- HW#7/main1.c
+++ HW#7/main1.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 
-int main(){
-    int n = 7;
-    int spc = n;
-    
-    
-    for(int i=1;i<=n;i++){
-        for(int k=spc;k>=1;k--){
-        printf(" ");
-    }
-    for(int j=1;j<=i;j++){
-        printf("%d ",i);
-    }
-    printf("\n");
-    spc--;
-    }
+int main()
+{
+    for (int i=1;i<8;i++){
+        printf("%*s",8-i," ");
+        for(int j=0;j<i;j++){
+            printf("%d ",i);
+        }
+        printf("\n");
+    } 
+    return 0;
 }
