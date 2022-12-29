f2215cfd1d6de1859f2105af967e4273f01f8295
B1129011 <119965133+B1129011@users.noreply.github.com>
2022-12-29 11:39:21 +0800

    Update main4.c

--- HW#7/main4.c
+++ HW#7/main4.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main() {
     int i = 12345, a, b, num;
-    a = (i/1000) - ((int)((i/1000)/10))*10;
-    b = i - ((int)(i/10))*10;
-    num = i - a*1000 -b +  b*1000 +a;
-    printf("%d",num);
+        a = (i/1000) - ((int)((i/1000)/10))*10;
+        b = i - ((int)(i/10))*10;
+        num = i - a*1000 -b +  b*1000 +a;
+        printf("%d",num);
+    
     return 0;
 }
